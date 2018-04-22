e87426a2b80e7cd36ebebb310f6c528f06a8f1b9
nemanjapetrovic <nemanjapetrovic1794@gmail.com>
2018-04-22 22:25:54 +0200

    Texture

---
+++ GL/Test_Texture/GLK/GLK.h
@@ -0,0 +1,33 @@
+
+// GLK.h : main header file for the GLK application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CGLKApp:
+// See GLK.cpp for the implementation of this class
+//
+
+class CGLKApp : public CWinApp
+{
+public:
+	CGLKApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CGLKApp theApp;
---
+++ GL/Test_Texture/GLK/GLRenderer.cpp
@@ -0,0 +1,340 @@
+#include "StdAfx.h"
+#include "GLRenderer.h"
+#include "GL\gl.h"
+#include "GL\glu.h"
+#include "GL\glaux.h"
+#include "GL\glut.h"
+#include "DImage.h"
+#include "Tex.h"
+//#pragma comment(lib, "GL\\glut32.lib")
+
+
+float vert[72];
+float tex[48];
+float tex2[48];
+
+CTex *brick_tex, *namotana_tex;
+
+CGLRenderer::CGLRenderer(void)
+{
+	brick_tex = new CTex();
+	namotana_tex = new CTex();
+}
+
+CGLRenderer::~CGLRenderer(void)
+{
+	brick_tex->Release();
+	delete brick_tex;
+	
+	namotana_tex->Release();
+	delete namotana_tex;
+}
+
+bool CGLRenderer::CreateGLContext(CDC* pDC)
+{
+    PIXELFORMATDESCRIPTOR pfd ;
+    memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+    pfd.nSize  = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion   = 1; 
+    pfd.dwFlags    = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;   
+    pfd.iPixelType = PFD_TYPE_RGBA; 
+    pfd.cColorBits = 32;
+    pfd.cDepthBits = 24; 
+    pfd.iLayerType = PFD_MAIN_PLANE;
+    
+    int nPixelFormat = ChoosePixelFormat(pDC->m_hDC, &pfd);
+    
+    if (nPixelFormat == 0) return false; 
+
+    BOOL bResult = SetPixelFormat (pDC->m_hDC, nPixelFormat, &pfd);
+    
+    if (!bResult) return false; 
+
+    m_hrc = wglCreateContext(pDC->m_hDC); 
+
+    if (!m_hrc) return false; 
+
+    return true;	
+}
+
+void CGLRenderer::PrepareScene(CDC *pDC)
+{
+    wglMakeCurrent(pDC->m_hDC, m_hrc);
+    //---------------------------------
+	
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
+
+	glLineWidth(2.0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	
+	PrepareCube(1.0);	
+
+	brick_tex->LoadTexture((CString)"brick.png");
+	namotana_tex->LoadTexture((CString)"env.png");
+
+	//---------------------------------
+    wglMakeCurrent(NULL, NULL);
+}
+
+void CGLRenderer::DrawScene(CDC *pDC)
+{
+    wglMakeCurrent(pDC->m_hDC, m_hrc);
+    //---------------------------------
+    
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(3.0, 3.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	
+	// Kocka obicna sa ciglama
+	brick_tex->PrepareTexture(false);
+	brick_tex->Select();	
+	DrawBrickCube();
+
+	glTranslatef(2.0, 0.0, 0.0);
+
+	// Kocka sa motanom teksturom
+	namotana_tex->PrepareTexture(false);
+	namotana_tex->Select();
+	DrawNamotanaCube();
+
+
+	glFlush();
+	SwapBuffers(pDC->m_hDC);
+    //---------------------------------	
+    wglMakeCurrent(NULL, NULL);
+}
+
+void CGLRenderer::Reshape(CDC *pDC, int w, int h)
+{
+    wglMakeCurrent(pDC->m_hDC, m_hrc);
+    //---------------------------------
+	
+
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	double aspect = (double)w / (double)h;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(40, aspect, 0.1, 100);
+	glMatrixMode(GL_MODELVIEW);
+
+
+    //---------------------------------
+    wglMakeCurrent(NULL, NULL);
+}
+
+void CGLRenderer::DestroyScene(CDC *pDC)
+{
+    wglMakeCurrent(pDC->m_hDC, m_hrc);
+    // ... 
+    wglMakeCurrent(NULL,NULL); 
+    if(m_hrc) 
+    {
+        wglDeleteContext(m_hrc);
+        m_hrc = NULL;
+    }
+}
+
+void CGLRenderer::PrepareCube(float a)
+{
+	//napred
+	vert[0] = -a / 2; vert[1] = -a / 2; vert[2] = a / 2; //0
+	vert[3] = a / 2; vert[4] = -a / 2; vert[5] = a / 2; //1
+	vert[6] = a / 2; vert[7] = a / 2; vert[8] = a / 2;//2
+	vert[9] = -a / 2; vert[10] = a / 2; vert[11] = a / 2;//3
+
+	//desno
+	vert[12] = a / 2; vert[13] = -a / 2; vert[14] = a / 2; //1
+	vert[15] = a / 2; vert[16] = a / 2; vert[17] = a / 2;//2
+	vert[18] = a / 2; vert[19] = a / 2; vert[20] = -a / 2;//5
+	vert[21] = a / 2; vert[22] = -a / 2; vert[23] = -a / 2;//4
+
+	//iza
+	vert[24] = a / 2; vert[25] = a / 2; vert[26] = -a / 2;//5
+	vert[27] = a / 2; vert[28] = -a / 2; vert[29] = -a / 2;//4
+	vert[30] = -a / 2; vert[31] = -a / 2; vert[32] = -a / 2;//7	
+	vert[33] = -a / 2; vert[34] = a / 2; vert[35] = -a / 2;//6
+	
+	//levo
+	vert[36] = -a / 2; vert[37] = -a / 2; vert[38] = -a / 2;//7	
+	vert[39] = -a / 2; vert[40] = a / 2; vert[41] = -a / 2;//6
+	vert[42] = -a / 2; vert[43] = a / 2; vert[44] = a / 2;//3
+	vert[45] = -a / 2; vert[46] = -a / 2; vert[47] = a / 2; //0
+
+	//gore
+	vert[48] = -a / 2; vert[49] = a / 2; vert[50] = a / 2;//3
+	vert[51] = a / 2; vert[52] = a / 2; vert[53] = a / 2;//2
+	vert[54] = a / 2; vert[55] = a / 2; vert[56] = -a / 2;//5
+	vert[57] = -a / 2; vert[58] = a / 2; vert[59] = -a / 2;//6
+
+	//dole
+	vert[60] = -a / 2; vert[61] = -a / 2; vert[62] = a / 2; //0
+	vert[63] = a / 2; vert[64] = -a / 2; vert[65] = a / 2; //1
+	vert[66] = a / 2; vert[67] = -a / 2; vert[68] = -a / 2;//4	
+	vert[69] = -a / 2; vert[70] = -a / 2; vert[71] = -a / 2;//7	
+
+
+	//texture
+
+	//napred
+	tex[0] = 0.0 ; tex[1] = 0.0;//0
+	tex[2] = 1.0;  tex[3] = 0.0;//1
+	tex[4] = 1.0; tex[5] = 1.0; //2
+	tex[6] = 0.0; tex[7] = 1.0; //3
+
+	//desno
+	tex[8] = 0.0; tex[9] = 0.0; //1
+	tex[10] =0.0; tex[11] = 1.0;//2
+	tex[12] = 1.1; tex[13] = 1.1;//5
+	tex[14] = 1.0; tex[15] = 0.0;//4
+	
+	//iza
+	tex[16] = 0.0; tex[17] = 1.0;//5
+	tex[18] = 0.0; tex[19] = 0.0; //4
+	tex[20] = 1.0; tex[21] =0.0; //7
+	tex[22] = 1.0; tex[23] = 1.1;//6
+
+	//levo
+	tex[24] = 0.0; tex[25] = 0.0; //7
+	tex[26] = 0.0; tex[27] = 1.0;//6
+	tex[28] = 1.0; tex[29] = 1.0;//3
+	tex[30] = 1.0; tex[31] = 0.0;//0
+
+	//gore
+	tex[32] = 0.0; tex[33] = 0.0;//3
+	tex[34] = 1.0; tex[35] = 0.0;//2												
+	tex[36] = 1.1; tex[37] = 1.1; //5
+	tex[38] = 0.0; tex[39] = 1.0;//6
+	
+	//dole
+	tex[40] = 0.0; tex[41] = 0.0;//0
+	tex[42] = 1.0; tex[43] = 0.0; //1
+	tex[44] = 1.1; tex[45] = 1.1; //4
+	tex[46] = 0.0; tex[47] = 1.0;	//7
+
+
+	//tex 2 - namotana
+
+	//napred
+	tex2[0] = 0.25; tex2[1] = 0.0;//0
+	tex2[2] = 0.5;  tex2[3] = 0.0;//1
+	tex2[4] = 0.5; tex2[5] =(float) 1/3;//2
+	tex2[6] = 0.25; tex2[7] = (float)1/3;//2
+
+	//desno
+	tex2[8] = 0.5; tex2[9] = (float)1/3; //1
+	tex2[10] = 0.5; tex2[11] = (float)2/3;//2
+	tex2[12] = 0.75; tex2[13] = (float)2/3;//5
+	tex2[14] = 0.75; tex2[15] = (float)1/3;//4
+
+	//iza
+	tex2[16] = 0.5; tex2[17] = 1;//5
+	tex2[18] = 0.5; tex2[19] = (float)2/3; //4
+	tex2[20] = 0.25; tex2[21] = (float)2/3; //7
+	tex2[22] = 0.25; tex2[23] = 1;//6
+
+	//levo
+	tex2[24] = 0.0; tex2[25] = (float)1/3; //7
+	tex2[26] = 0.0; tex2[27] = (float)2/3;//6
+	tex2[28] = 0.25; tex2[29] = (float)2/3;//3
+	tex2[30] = 0.25; tex2[31] = (float)1/3;//0
+
+	//gore
+	tex2[32] = 0.75; tex2[33] = (float)1/3;//3
+	tex2[34] = 1.0; tex2[35] = (float)1/3;//2												
+	tex2[36] = 1.0; tex2[37] = (float)2/3; //5
+	tex2[38] = 0.75; tex2[39] = (float)2/3;//6
+
+	//dole
+	tex2[40] = 0.25; tex2[41] = (float)1/3;//0
+	tex2[42] = 0.5; tex2[43] = (float)1/3; //1
+	tex2[44] = 0.5; tex2[45] = (float)2/3; //4
+	tex2[46] = 0.25; tex2[47] = (float)2/3;//7
+}
+
+void CGLRenderer::DrawBrickCube()
+{
+	DrawAxes(2.0);
+	
+	glVertexPointer(3, GL_FLOAT, 0, vert);	
+	glTexCoordPointer(2, GL_FLOAT, 0, tex);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	glDrawArrays(GL_QUADS, 0, 24);
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
+
+void CGLRenderer::DrawNamotanaCube()
+{
+	DrawAxes(2.0);
+
+	glVertexPointer(3, GL_FLOAT, 0, vert);
+	glTexCoordPointer(2, GL_FLOAT, 0, tex2);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	glDrawArrays(GL_QUADS, 0, 24);
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
+
+void CGLRenderer::DrawAxes(float size)
+{
+	glBegin(GL_LINES);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(size, 0.0, 0.0);
+
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(0.0, size, 0.0);
+
+	glColor3f(1.0, 0.0, 1.0);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(0.0, 0.0, size);
+
+	glEnd();
+}
+
+double CGLRenderer::R(double a, double alpha, int n, double h)
+{
+    alpha *= 0.01745329251994329577;
+    double PI2 = 6.2831853071795864769;
+    double alpha1 = (PI2 / (2 * n) - alpha);
+    double alpha2;
+    double R1, R;
+
+    R = a / (2 * sin(alpha1 / 2.0));
+    double delta = R / 4.;
+    R1 = R;
+    do{
+        alpha1 = 2 * asin(a / (2 * R1));
+        alpha2 = 2 * asin(a / (2 * (R1 - h)));
+        if ((alpha1 + alpha2) > ((PI2 / n) - (2 * alpha)))
+            R1 += delta;
+        else
+            R1 -= delta;
+        delta /= 2.0;
+    } while (delta > 1e-6);
+    return R;
+}
+
+void CGLRenderer::NormCrossProd(double x1, double y1, double z1, double x2, double y2, double z2, double& x, double& y, double& z)
+{
+    x = y1 * z2 - z1 * y2;
+    y = z1 * x2 - x1 * z2;
+    z = x1 * y2 - y1 * x2;
+    double d = sqrt(x*x + y*y + z*z);
+    if (d != 0.0)
+    {
+        x /= d; y /= d; z /= d;
+    }
+}
